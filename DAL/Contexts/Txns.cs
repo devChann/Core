94ad9158cad77d59f1cd36d58a0ab92f1d978aac
Chann <princechanns@gmail.com>
2020-07-09 13:01:38 +0300

    modify txns entity

--- DAL/Contexts/Txns.cs
+++ DAL/Contexts/Txns.cs
@@ -12,8 +12,8 @@ namespace DAL.Contexts
         public string Breed { get; set; }
         public string BreedGender { get; set; }
         public int? Quantity { get; set; }
-        public double Revenue { get; set; }
-        public int AmtofMilkdp { get; set; }
+        public string Revenue { get; set; }
+        public string AmtofMilkdp { get; set; }
         public string Age { get; set; }
        // public string TransactionId { get; set; }
         public string TransactionsId { get; set; }
